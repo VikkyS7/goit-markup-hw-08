9b902949fac02ba814a865cc858f03d122624ddc
Victoria Shcherbakova <vik.shcherbakova@gmail.com>
2022-11-28 03:08:12 +0300

    hw_08_most

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,23 @@
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
+      openModalMobileMenuBtn: document.querySelector("[data-burger]"),
+      closeModalMobileMenuBtn: document.querySelector("[data-burger-close]"),
+      modalMobileMenu: document.querySelector("[data-burger-menu]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalMobileMenuBtn.addEventListener("click", toggleModal.bind("mob"));
+  refs.closeModalMobileMenuBtn.addEventListener("click", toggleModal.bind("mob"));
   
-    function toggleModal() {
+  function toggleModal(modal="default") {
+    if (modal !== 'default') {
+      refs.modalMobileMenu.classList.toggle("is-hidden");
+        
+    }
+    else {
       refs.modal.classList.toggle("is-hidden");
+    }
     }
   })();
